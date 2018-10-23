8bb9d33ce3d0706472e0e95b59b7a3d84e27c25e
roryjackd <roryjackd@yahoo.com>
2018-10-23 17:06:05 -0600

    Added logo

--- src/components/Navigation/ToolBar/Toolbar.js
+++ src/components/Navigation/ToolBar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
-//import Logo from '../../Logo/Logo';
+import Logo from '../../Logo/Logo';
 //import NavigationItems from '../NavigationItems/NavigationItems';
 //import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
