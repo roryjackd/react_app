a283664d72a047eb4b05bb8416cb207d0e87e20f
roryjackd <roryjackd@yahoo.com>
2018-10-24 18:08:09 -0600

    File refactor

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/ToolBar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/ToolBar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
