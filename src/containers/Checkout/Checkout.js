54f7189cb3f5ef0ec742dabba4244a09c45df7ea
roryjackd <roryjackd@yahoo.com>
2018-10-27 14:54:47 -0600

    Set up routing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import ContactData from './ContactData/ContactData';
+//import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -33,19 +33,19 @@ class Checkout extends Component {
         this.props.history.replace( '/checkout/contact-data' );
     }
 
-    render () {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.state.ingredients}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
-                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
-            </div>
-        );
-    }
+    // render () {
+    //     return (
+    //         <div>
+    //             <CheckoutSummary
+    //                 ingredients={this.state.ingredients}
+    //                 checkoutCancelled={this.checkoutCancelledHandler}
+    //                 checkoutContinued={this.checkoutContinuedHandler} />
+    //             <Route 
+    //                 path={this.props.match.path + '/contact-data'} 
+    //                 render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
+    //         </div>
+    //     );
+    // }
 }
 
 export default Checkout;
